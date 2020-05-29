c65ec9db105f15792efdd420bbaad2be2b2b9384
Emre <eaydin@verdazo.com>
2020-05-29 19:28:09 +0300

    Initial push with layout start

--- src/ts/scene.ts
+++ src/ts/scene.ts
@@ -1,29 +1,17 @@
-import {
-  Color,
-  PerspectiveCamera,
-  Scene,
-  Vector3,
-  WebGLRenderer,
-  Geometry,
-  MeshStandardMaterial,
-  MeshBasicMaterial,
-  CubeCamera,
-  BoxGeometry,
-  Mesh,
-} from "three";
-import "../css/styles.scss";
+import '../css/styles.scss';
+
+import {BoxGeometry, Color, CubeCamera, Geometry, Mesh, MeshBasicMaterial, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer,} from 'three';
 
 export class App {
+  private canvas = document.getElementById('main-canvas')! as HTMLCanvasElement;
+
+
   private readonly scene = new Scene();
   private readonly camera = new PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+      45, this.canvas.clientWidth / this.canvas.clientHeight, 0.1, 1000);
   private readonly renderer = new WebGLRenderer({
     antialias: true,
-    canvas: document.getElementById("main-canvas") as HTMLCanvasElement,
+    canvas: this.canvas,
   });
 
   private geoCube = new BoxGeometry(1, 1, 1);
@@ -36,23 +24,40 @@ export class App {
     this.camera.position.set(10, 10, 10);
     this.camera.lookAt(new Vector3(0, 0, 0));
 
-    this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new Color("rgb(0,0,0)"));
+    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
 
     this.scene.add(this.meshCube);
 
     this.render();
   }
 
+  private resizeRendererToDisplaySize() {
+    const rendererDom = this.renderer.domElement;
+    const canvasWidth = this.canvas.clientWidth;
+    const canvasHeight = this.canvas.clientHeight;
+
+    const needResize = rendererDom.width !== canvasWidth ||
+        rendererDom.height !== canvasHeight;
+    if (needResize) {
+      console.log('needs resize');
+      this.renderer.setSize(canvasWidth, canvasHeight, false);
+    }
+    return needResize;
+  }
+
   private adjustCanvasSize() {
-    this.renderer.setSize(innerWidth, innerHeight);
-    this.camera.aspect = innerWidth / innerHeight;
-    this.camera.updateProjectionMatrix();
+    if (this.resizeRendererToDisplaySize()) {
+      const rendererDom = this.renderer.domElement;
+      this.camera.aspect = rendererDom.clientWidth / rendererDom.clientHeight;
+      this.camera.updateProjectionMatrix();
+    }
   }
 
   private render() {
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(() => this.render());
+
+    this.meshCube.rotation.y += 0.1;
 
     this.adjustCanvasSize();
   }
